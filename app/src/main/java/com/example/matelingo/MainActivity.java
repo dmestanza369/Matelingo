96e1bf0239b313563ecfab63dfed2ab8b2c5624f
Azahara Andújar <49288113+azzzahara@users.noreply.github.com>
2021-04-17 12:45:44 +0200

    Página Principal
    Oleee

--- app/src/main/java/com/example/matelingo/MainActivity.java
+++ app/src/main/java/com/example/matelingo/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.matelingo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button botonAcceder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        botonAcceder = (Button) findViewById(R.id.botonacceder);
+
+        botonAcceder.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Principal.class);
+                setContentView(R.layout.principal);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
